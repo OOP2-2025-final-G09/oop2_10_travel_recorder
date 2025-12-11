d84d0a4f8c65c6fe893142779163676c0613f826
yusei <youshengjiubaotian668@gmail.com>
2025-12-11 17:18:20 +0900

    エラーの修正

--- routes/order.py
+++ routes/order.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from models import Order, User, Place
+from models import Order, User, Product
 from datetime import datetime
 
 # Blueprintの作成
@@ -9,21 +9,21 @@ order_bp = Blueprint('order', __name__, url_prefix='/orders')
 @order_bp.route('/')
 def list():
     orders = Order.select()
-    return render_template('order_list.html', title='旅行記録一覧', items=orders)
+    return render_template('order_list.html', title='注文一覧', items=orders)
 
 
 @order_bp.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
         user_id = request.form['user_id']
-        place_id = request.form['place_id']
+        product_id = request.form['product_id']
         order_date = datetime.now()
-        Order.create(user=user_id, place=place_id, order_date=order_date)
+        Order.create(user=user_id, product=product_id, order_date=order_date)
         return redirect(url_for('order.list'))
     
     users = User.select()
-    places = Place.select()
-    return render_template('order_add.html', users=users, places=places)
+    products = Product.select()
+    return render_template('order_add.html', users=users, products=products)
 
 
 @order_bp.route('/edit/<int:order_id>', methods=['GET', 'POST'])
@@ -35,14 +35,9 @@ def edit(order_id):
     if request.method == 'POST':
         order.user = request.form['user_id']
         order.product = request.form['product_id']
-
-        date_str = request.form['order_date']
-        order.order_date = datetime.strptime(date_str, "%Y-%m-%d")
-
         order.save()
         return redirect(url_for('order.list'))
 
     users = User.select()
-
     products = Product.select()
     return render_template('order_edit.html', order=order, users=users, products=products)
