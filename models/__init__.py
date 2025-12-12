65435434513db9ffa295c7945177d81263f95a4b
kazuki404313 <kazuki20050404@gmail.com>
2025-12-12 16:27:25 +0900

    全て動くように修正・追加をしました

--- models/__init__.py
+++ models/__init__.py
@@ -2,12 +2,17 @@ from peewee import SqliteDatabase
 from .db import db
 from .user import User
 from .product import Product
+from .company import Company
 from .order import Order
+
+from .place import Place
 
 # モデルのリストを定義しておくと、後でまとめて登録しやすくなります
 MODELS = [
     User,
     Product,
+    Company,
+    Place,
     Order,
 ]
 
--- routes/__init__.py
+++ routes/__init__.py
@@ -1,10 +1,15 @@
 from .user import user_bp
 from .product import product_bp
+from .company import company_bp
 from .order import order_bp
+from .place import place_bp
+
 
 # Blueprintをリストとしてまとめる
 blueprints = [
   user_bp,
   product_bp,
-  order_bp
+  company_bp,
+  order_bp,
+  place_bp
 ]
