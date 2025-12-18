d1ce5cd549bf061f00753dac59c197da99183a0d
こぽー <tora1025oishi@gmail.com>
2025-12-18 12:25:34 +0900

    ダッシュボード実装用のファイル構造を追加
    - routes/dashboard.py: データ集計API用のスケルトン追加
    - static/dashboard.js: Chart.js実装用のファイル追加
    - templates/index.html: ダッシュボード用のTODOコメント追加
    - models/order.py: Traveler -> travelerにフィールド名を修正

---
+++ static/dashboard.js
@@ -0,0 +1,18 @@
+// ダッシュボードのグラフ描画
+// 担当C: このファイルを実装
+
+// Chart.jsを使って3つのグラフを描画する
+// 1. 利用年齢層 - 棒グラフ
+// 2. 人気の旅行先 - 円グラフ
+// 3. 利用が多かった日 - 棒グラフ
+
+// TODO: Chart.jsのCDNをindex.htmlに追加
+// TODO: 各グラフの描画関数を実装
+// TODO: APIからデータを取得してグラフに反映
+
+// 例:
+// fetch('/api/age_distribution')
+//   .then(response => response.json())
+//   .then(data => {
+//     // Chart.jsで棒グラフを描画
+//   });
